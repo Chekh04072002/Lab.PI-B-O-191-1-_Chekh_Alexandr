cc2e29642c525f44c3eb3e1e0c4af10230b303e8
Chekh04072002 <55432401+Chekh04072002@users.noreply.github.com>
2019-10-09 08:58:52 +0300

    Add files via upload

---
+++ pr013/pr013/pr013.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+int main() {
+	setlocale(LC_ALL, "russian");
+	long long  n, p = 1, sum = 1;
+	std::cout << "Введите число" << std::endl;
+	std::cin >> n;
+	if (n == 0) {
+		std::cout << 0 << std::endl;
+	}
+	else
+	{
+		while ((p *= 2) <= n)
+			sum++;
+		std::cout << sum << std::endl;
+	}
+	return 0;
+
+}
