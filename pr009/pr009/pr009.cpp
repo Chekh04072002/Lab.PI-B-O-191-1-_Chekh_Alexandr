44763bf318d216b70f91609b72bd467259c3af1c
Chekh04072002 <55432401+Chekh04072002@users.noreply.github.com>
2019-10-09 09:03:39 +0300

    Add files via upload

---
+++ pr009/pr009/pr009.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL) );
+link:
+	int namber, namber1, vib;
+	std::cout << "Игра началась. Угадывайте число от 1 до 100" << std::endl;
+	namber = rand() %
+		100;
+	for (int i = 0; i < 5; i++) {
+		std::cin >> namber1;
+		if (namber1 != namber) {
+			if (namber1 > namber) {
+				std::cout << "Загаданное число меньше" << std::endl;
+			}
+			else {
+				std::cout << "Загаданное число больше" << std::endl;
+			}
+		}
+		else {
+			std::cout << "Поздравляю! Вы угадали!" << std::endl;
+		}
+	}
+	std::cout << "Вы проиграли. Было загадано число:" << namber << std::endl;
+	std::cout << "Хотите начать сначала? (1-Да)" << std::endl;
+	std::cin >> vib;
+	if (vib == 1) {
+		goto link;
+	}
+	else {
+		return 0;
+	}
+}
