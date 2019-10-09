3e848a8ccdf44302a57901cb1b8313ec448a09b5
Chekh04072002 <55432401+Chekh04072002@users.noreply.github.com>
2019-10-09 09:01:16 +0300

    Add files via upload

---
+++ pr008/pr008/pr008.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b;
+	char c;
+	cout << "Введите первое вещественное число" << endl;
+	cin >> a;
+	cout << "Введите символ-знак операции" << endl;
+	cin >> c;
+	cout << "Введите второе вещественное число" << endl;
+	cin >> b;
+	if (c == '+') {
+		cout << a + b << endl;
+	}
+	else if (c == '*') {
+		cout << a * b << endl;
+	}
+	else if (c == '/') {
+		cout << a / b << endl;
+	}
+	else cout << "Неправильный ввод" << endl;
+	return 0;
+}
