93dcf991e8b5e458193b3ab9cb01204c26b28f91
Chekh04072002 <55432401+Chekh04072002@users.noreply.github.com>
2019-10-09 08:55:55 +0300

    Add files via upload

---
+++ pr012/pr012/pr012.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "russian");
+
+	int a, b;
+	std::cout << "Введите число" << std::endl;
+	std::cin >> a;
+	for (int i = a - 1; i > 1; i--) {
+		if (a % i == 0) {
+			std::cout << "Число составное" << std::endl;
+
+			return 0;
+		}
+
+	}
+	std::cout << "Число простое" << std::endl;
+	return 0;
+
+}
