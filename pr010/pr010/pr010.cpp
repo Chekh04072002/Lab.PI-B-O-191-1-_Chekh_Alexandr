5e491f70afde9706a015731c5194ebed649a1879
Chekh04072002 <55432401+Chekh04072002@users.noreply.github.com>
2019-10-09 08:37:54 +0300

    Add files via upload

---
+++ pr010/pr010/pr010.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+int main() {
+	setlocale(LC_ALL, "Russian");
+	double A = 0;
+	int k = 0;
+	double r = 1;
+	std::cout << "Введите число " << std::endl;
+	std::cin >> A;
+	std::cout << "Введите целую степень " << std::endl;
+	std::cin >> k;
+	for (int i = 0; i < k; i++) {
+		r = r * A;
+	}
+	std::cout << "Ответ:" << " " << r << std::endl;
+
+	return 0;
+}
