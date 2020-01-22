412d99f43c23b631f280a67c2d09d7477673cd01
Chekh04072002 <55432401+Chekh04072002@users.noreply.github.com>
2020-01-22 21:51:02 +0300

    Update 23.cpp

--- 23/23/23.cpp
+++ 23/23/23.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 bool Chislo(int i)
 {
@@ -18,22 +18,22 @@ int main()
 	char first[21], bez_pro[21] = { 0 };
 	std::cin.getline(first, 21);
 
-	for (int i = 0, n = 0; i < strlen(first); i++)
+	for (int i = 0, n = 0; i < strlen(first); i++) // Проверяются на подходящие символы и заносятся во второй вектор без пробелов
 		if (Chislo((int)first[i]) || Bukva((int)first[i])) {
 			bez_pro[n] = first[i];
 			n++;
 		}
 
 	bool ne_pal = true;
-	int length = strlen(bez_pro);
+	int length = strlen(bez_pro); // Длина
 	if (length) {
-		for (int l = 0; l < length / 2; l++) {
+		for (int l = 0; l < length / 2; l++) { // середина - граница, -1 и -l, так как проверяется на палиндром соответствующие элементы от начала и с конца
 			if (Chislo((int)bez_pro[l]) && bez_pro[l] != bez_pro[length - 1 - l]) {
 				ne_pal = false;
 				break;
 			}
 
-			else if ((int)bez_pro[l] != (int)bez_pro[length - 1 - l] && (int)bez_pro[l] + 32 != (int)bez_pro[length - 1 - l] && (int)bez_pro[l] - 32 != (int)bez_pro[length - 1 - l]) {
+			else if ((int)bez_pro[l] != (int)bez_pro[length - 1 - l] && (int)bez_pro[l] + 32 != (int)bez_pro[length - 1 - l] && (int)bez_pro[l] - 32 != (int)bez_pro[length - 1 - l]) { // +32 и -32, так как проверяются буквы в разном реестре и все они подходят
 				ne_pal = false;
 				break;
 			}
@@ -43,4 +43,3 @@ int main()
 	if (ne_pal) std::cout << "Палиндром";
 	else std::cout << "Не палиндром";
 }
-
